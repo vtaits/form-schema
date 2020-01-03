d5df5b0016dc5240c5913d2e89c963f9194bc052
Vadim Taits <taizvadim@gmail.com>
2020-01-03 19:11:39 +0300

    Added example with serializer and parser

---
+++ packages/react-final-form-schema/src/__stories__/SerializerAndParser.jsx
@@ -0,0 +1,212 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+
+import { useField } from 'react-final-form';
+
+import { Form } from '../index';
+
+const SelectComponent = ({
+  fieldUniq,
+
+  fieldSchema: {
+    label,
+    options,
+  },
+}) => {
+  const {
+    input: {
+      value,
+      onChange,
+    },
+
+    meta: {
+      dirtySinceLastSubmit,
+      submitError,
+    },
+  } = useField(fieldUniq);
+
+  return (
+    <div>
+      {
+        label && (
+          <p>
+            {label}
+          </p>
+        )
+      }
+
+      <Select
+        isClearable
+        name={fieldUniq}
+        value={value || null}
+        options={options}
+        onChange={onChange}
+      />
+
+      {
+        !dirtySinceLastSubmit && submitError && (
+          <ul
+            style={{
+              color: 'red',
+            }}
+          >
+            {
+              submitError.map((message, index) => (
+                <li key={index}>
+                  {message}
+                </li>
+              ))
+            }
+          </ul>
+        )
+      }
+    </div>
+  );
+};
+
+SelectComponent.propTypes = {
+  fieldUniq: PropTypes.string.isRequired,
+
+  fieldSchema: PropTypes.shape({
+    label: PropTypes.string,
+    options: PropTypes.arrayOf(PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired,
+    })).isRequired,
+  }).isRequired,
+};
+
+const fieldTypes = {
+  select: {
+    component: SelectComponent,
+
+    serializer: (values, name) => {
+      const value = values[name];
+
+      return {
+        [name]: value ? value.value : null,
+      };
+    },
+
+    parser: (values, name, { options }) => {
+      const value = values[name];
+
+      return {
+        [name]: value
+          ? options.find((option) => {
+            if (option.value === value) {
+              return true;
+            }
+
+            return false;
+          })
+          : null,
+      };
+    },
+  },
+};
+
+const getFieldType = ({ type }) => fieldTypes[type];
+
+const fullSchema = {
+  animal: {
+    type: 'select',
+    label: 'Animal',
+
+    options: [
+      {
+        value: 1,
+        label: 'Cat',
+      },
+
+      {
+        value: 2,
+        label: 'Dog',
+      },
+
+      {
+        value: 3,
+        label: 'Elephant',
+      },
+
+      {
+        value: 4,
+        label: 'Cow',
+      },
+    ],
+  },
+};
+
+const getFieldSchema = (fieldName) => fullSchema[fieldName];
+
+const schema = ['animal'];
+
+const delay = (ms) => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, ms);
+});
+
+const initialValues = {
+  animal: 2,
+};
+
+const Example = () => {
+  const [submittedValues, setSubmittedValues] = useState(null);
+
+  const onSubmit = async (values) => {
+    await delay(1000);
+
+    setSubmittedValues(values);
+  };
+
+  return (
+    <>
+      <Form
+        initialValues={initialValues}
+        getFieldSchema={getFieldSchema}
+        getFieldType={getFieldType}
+        schema={schema}
+        onSubmit={onSubmit}
+      >
+        {({
+          handleSubmit,
+          submitting,
+          renderField,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            {renderField('animal')}
+
+            <hr />
+
+            <button
+              type="submit"
+              disabled={submitting}
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </Form>
+
+      {
+        submittedValues && (
+          <>
+            <hr />
+
+            <h3>
+              Submitted values:
+            </h3>
+
+            <pre>
+              {JSON.stringify(submittedValues, null, 2)}
+            </pre>
+          </>
+        )
+      }
+    </>
+  );
+};
+
+export default Example;
--- packages/react-final-form-schema/src/__stories__/index.stories.js
+++ packages/react-final-form-schema/src/__stories__/index.stories.js
@@ -1,6 +1,8 @@
 import { storiesOf } from '@storybook/react';
 
 import Simple from './Simple';
+import SerializerAndParser from './SerializerAndParser';
 
 storiesOf('react-final-form-schema')
-  .add('Simple', Simple);
+  .add('Simple', Simple)
+  .add('Serializer and parser', SerializerAndParser);
