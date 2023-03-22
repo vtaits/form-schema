b700e1e591aa30b88eb2de4391a6832e0583913f
Vadim Taits <taizvadim@gmail.com>
2023-03-22 16:53:53 +0300

    Migrate to `tsup`

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,47 +1,18 @@
 module.exports = {
   stories: ['../packages/**/*.stories.mdx'],
 
+  framework: '@storybook/react',
+
   addons: [
+    '@storybook/preset-create-react-app',
     '@storybook/addon-docs',
   ],
 
-  webpackFinal: (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
-      options: {
-        presets: [
-          '@babel/preset-typescript',
-          [
-            '@babel/env', {
-              modules: false,
+  core: {
+    builder: '@storybook/builder-vite',
+  },
 
-              targets: {
-                firefox: '91',
-                chrome: '92',
-              },
-            },
-          ],
-
-          [
-            '@babel/preset-react',
-            {
-              runtime: 'automatic',
-            },
-          ],
-        ],
-
-        plugins: [
-          // https://github.com/babel/babel/issues/10261
-          ['@babel/plugin-transform-runtime', {
-            version: '7.15.3',
-          }],
-        ],
-      },
-    });
-
-    config.resolve.extensions.push('.ts', '.tsx');
-
-    return config;
+  features: {
+    storyStoreV7: true,
   },
 };
