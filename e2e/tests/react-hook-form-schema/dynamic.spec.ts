e055cffd189acc276194c566a20e169eb14730aa
Vadim Taits <taizvadim@gmail.com>
2025-01-09 16:15:08 +0300

    WIP: playwright

---
+++ e2e/tests/react-hook-form-schema/dynamic.spec.ts
@@ -0,0 +1,6 @@
+import { expect, test } from "@playwright/test";
+import { createMakeUrl } from "tests/utils/makeUrl";
+
+test.fixme("parse initial value correctly", async ({ page }) => {});
+
+test.fixme("change and submit", async ({ page }) => {});
