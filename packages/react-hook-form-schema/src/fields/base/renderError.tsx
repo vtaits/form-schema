fef835e75af47e018d6f99b1d9de4c923b609dcd
Vadim Taits <taizvadim@gmail.com>
2025-02-05 13:18:56 +0300

    [react-hook-form-schema] support array errors

--- packages/react-hook-form-schema/src/fields/base/renderError.tsx
+++ packages/react-hook-form-schema/src/fields/base/renderError.tsx
@@ -1,5 +1,12 @@
 import type { ReactNode } from "react";
 
+function renderArray(errors: readonly unknown[]): ReactNode {
+	return errors.map((errorItem, index) => (
+		// biome-ignore lint/suspicious/noArrayIndexKey: there is no id
+		<div key={index}>{renderError(errorItem)}</div>
+	));
+}
+
 export function renderError(error: unknown): ReactNode {
 	if (!error) {
 		return null;
@@ -9,8 +16,18 @@ export function renderError(error: unknown): ReactNode {
 		return error;
 	}
 
-	if (typeof (error as Record<string, unknown>).message === "string") {
-		return (error as Record<string, string>).message;
+	if (Array.isArray(error)) {
+		return renderArray(error);
+	}
+
+	const message = (error as Record<string, unknown>).message;
+
+	if (typeof message === "string") {
+		return message;
+	}
+
+	if (Array.isArray(message)) {
+		return renderArray(message);
 	}
 
 	return null;
